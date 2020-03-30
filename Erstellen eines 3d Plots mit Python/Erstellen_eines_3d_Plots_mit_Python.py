6486286351e6ce164fc811c0e4cf93b855594adc
EMT Labor <emtlabor@outlook.de>
2020-03-30 15:53:26 +0200

    Darzustellende Funktion definieren und erstellen

--- Erstellen eines 3d Plots mit Python/Erstellen_eines_3d_Plots_mit_Python.py
+++ Erstellen eines 3d Plots mit Python/Erstellen_eines_3d_Plots_mit_Python.py
@@ -2,3 +2,12 @@ import mpl_toolkits
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+
+def f(x, y):
+    return np.sin(np.sqrt(x ** 2 + y ** 2))
+
+x = np.linspace(-7, 7, 30)
+y = np.linspace(-7, 7, 30)
+
+X, Y = np.meshgrid(x, y)
+Z = f(X, Y)
