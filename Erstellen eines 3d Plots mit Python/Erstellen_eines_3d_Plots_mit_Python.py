d8a7224aa0c6cfea13eea1bad61ddc427b7fb66b
EMT Labor <emtlabor@outlook.de>
2020-03-30 15:55:00 +0200

    Fenster für die Ausgabe und Befehl zur Ausgabe erstellt

--- Erstellen eines 3d Plots mit Python/Erstellen_eines_3d_Plots_mit_Python.py
+++ Erstellen eines 3d Plots mit Python/Erstellen_eines_3d_Plots_mit_Python.py
@@ -11,3 +11,12 @@ y = np.linspace(-7, 7, 30)
 
 X, Y = np.meshgrid(x, y)
 Z = f(X, Y)
+
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+ax.contour3D(X, Y, Z, 50, cmap='binary')
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_zlabel('z')
+
+plt.show()
